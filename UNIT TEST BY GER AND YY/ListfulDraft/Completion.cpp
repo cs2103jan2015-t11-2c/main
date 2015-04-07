7226f38a8ef42ceaad546db04b1bc6f532c16df6
lim theck yee <limtheckyee@gmail.com>
2015-04-07 18:27:23 +0800

    Check Complete

--- UNIT TEST BY GER AND YY/ListfulDraft/Completion.cpp
+++ UNIT TEST BY GER AND YY/ListfulDraft/Completion.cpp
@@ -1,8 +1,12 @@
 #include "Completion.h"
 
 void updateCompleted(DataStore &data, int index){
-	
+	data.getTempData().clear();
+
 	data.getData()[index].isComplete = true;
+	data.getTempData().push_back(data.getData()[index]);
+	data.getData().erase(data.getData().begin()+index);
+	data.getData().insert(data.getData().end(), data.getTempData()[0]);
 
 	return;
 }
