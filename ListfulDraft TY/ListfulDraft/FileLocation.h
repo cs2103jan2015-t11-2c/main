ac78e9abe0ed7c52363e8ee4e55837b3faefc681
lim theck yee <limtheckyee@gmail.com>
2015-04-08 22:47:05 +0800

    unit test (addFloating, addScheduled, addDeadline & delete)

---
+++ ListfulDraft TY/ListfulDraft/FileLocation.h
@@ -0,0 +1,25 @@
+#ifndef FILELOCATION_H
+#define FILELOCATION_H
+#include "Parser.h"
+
+class FileLocation {
+private:
+	static const std::string FILE_LOCATION_LIST;
+
+	enum fileMsg {
+		OPEN, CREATE
+	};
+
+	std::string _fileName;
+
+public:
+	FileLocation() {};
+	
+	void saveFileLocation();
+	bool findFile(DataStore &);
+	int openFile(DataStore &, Parser, Classes &);
+
+	std::string &getName();
+};
+
+#endif
---
+++ ListfulDraft TY/ListfulDraft/UserInterface.h
@@ -0,0 +1,38 @@
+#ifndef USERINTERFACE_H
+#define USERINTERFACE_H
+
+#include "FileLocation.h"
+#include "UserMessage.h"
+
+#define MAX 1000
+
+class UserInterface {
+	private:
+		std::string _userInput;
+
+		std::string _outputToUser;
+
+		int i;
+
+		char msgToUser[MAX];
+
+	public:
+		UserInterface() {};
+
+		void centralizePrintToUser(std::string, std::ostringstream &);
+		void runProgram();
+		void clearData(DataStore &, std::ostringstream &, std::ostringstream &, std::ostringstream &, std::ostringstream &);
+		void readFileName(std::string &, UserMessage);
+		void showReminder(DataStore, Classes &, std::string &, std::ostringstream &, std::ostringstream &, std::ostringstream &, UserMessage, bool);
+		std::string getOutputToUser(DataStore, std::string, std::string, std::ostringstream &, std::ostringstream &, std::ostringstream &, std::ostringstream &, UserMessage, bool);
+		void determineOutput(DataStore &, std::string, int);
+		void startUpScreen(DataStore &, Classes &, FileLocation &, Parser &, std::string &, std::string &, UserMessage , std::ostringstream &, std::ostringstream &, std::ostringstream &, std::ostringstream &, bool);
+		void extractFileName(std::string &, std::string &, FileLocation &);
+		std::string getPath();
+		std::string quoteOfTheDay(UserMessage);
+		std::string getCurrent(UserMessage, int = 0);
+		void outputCommand(UserMessage);
+		void homeScreen(UserMessage, Classes &, std::string);
+};
+
+#endif
